dda4019cd9e8e346c477ae16e34f3a19957e8b16
Nat2ume <benoitnathael@gmail.com>
2025-03-20 22:25:56 +0100

    optimisation

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,3 @@
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
-    var nav = document.querySelector('nav');
-    var contenulistes = document.querySelectorAll('.contenuliste');
-
-    if (window.scrollY > header.offsetHeight) {
-        nav.classList.add('fixed-nav');
-        contenulistes.forEach(list => {
-            list.classList.add('fixed-contenuliste');
-            list.style.top = nav.offsetHeight + 'px';
-        });
-    } else {
-        nav.classList.remove('fixed-nav');
-        contenulistes.forEach(list => {
-            list.classList.remove('fixed-contenuliste');
-            list.style.top = 'auto';
-        });
-    }
-});
-
 document.querySelectorAll('.bouton').forEach(button => {
     button.addEventListener('click', function() {
         var targetDetails = document.getElementById(this.getAttribute('data-details'));
@@ -25,7 +5,7 @@ document.querySelectorAll('.bouton').forEach(button => {
     });
 });
 
-document.querySelectorAll('.project-details .close').forEach(closeButton => {
+document.querySelectorAll('.popup-details .close').forEach(closeButton => {
     closeButton.addEventListener('click', function() {
         var targetDetails = this.parentElement;
         targetDetails.classList.remove('visible');
